fb28780bf24df67c26e365550731250e6153de8c
宋印赠 <songyinzeng@gmail.com>
2019-06-13 18:04:24 +0800

    国际化，流程图菜单

--- src/router/modules/bpmn.js
+++ src/router/modules/bpmn.js
@@ -3,34 +3,35 @@
 import Layout from '@/views/layout/Layout'
 
 const bpmnRouter = {
-  path: '/bpmn',
-  component: Layout,
-  redirect: '/bpmn/create',
-  name: '流程图',
-  meta: {
-    title: '流程图',
-    icon: 'tree'
-  },
-  children: [
-    {
-      path: 'create',
-      component: () => import('@/views/bpmn/create'), // Parent router-view
-      name: '创建流程图',
-      meta: {
-        title: '创建流程图',
-        icon: 'tree'
-      }
+    path: '/bpmn',
+    component: Layout,
+    redirect: '/bpmn/create',
+    name: 'Bpmn',
+    meta: {
+        title: 'bpmn',
+        icon: 'bpmn-io'
     },
-    {
-      path: 'update',
-      component: () => import('@/views/bpmn/update'),
-      name: '修改流程图',
-      meta: {
-        title: '修改流程图',
-        icon: 'tree'
-      }
-    }
-  ]
+    children: [{
+            path: 'create',
+            component: () =>
+                import ('@/views/bpmn/create'), // Parent router-view
+            name: 'CreateBpmn',
+            meta: {
+                title: 'createBpmn',
+                icon: 'tree'
+            }
+        },
+        {
+            path: 'watch',
+            component: () =>
+                import ('@/views/bpmn/watch'),
+            name: 'WatchBpmn',
+            meta: {
+                title: 'watchBpmn',
+                icon: 'open-eye'
+            }
+        }
+    ]
 }
 
 export default bpmnRouter
